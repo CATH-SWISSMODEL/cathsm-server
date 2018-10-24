c72b8575c6a0a02bfccde888318fec264eb1f240
sillitoe <ian@sillit.com>
2018-10-24 22:12:23 +0100

    lint

--- api/views.py
+++ api/views.py
@@ -1,18 +1,25 @@
+"""CATH API Views"""
+
+import logging
+
 from django.shortcuts import render
 
-# Create your views here.
-
 from rest_framework import generics
+
 from .serializers import SelectTemplateQuerySerializer, SelectTemplateResultsSerializer
 from .models import SelectTemplateTask
 
-class SelectTemplateTaskCreateView(generics.ListCreateAPIView):
+LOG = logging.getLogger(__name__)
+
+class SelectTemplateTaskCreateView(generics.CreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = SelectTemplateTask.objects.all()
     serializer_class = SelectTemplateQuerySerializer
 
     def perform_create(self, serializer):
         """Save the post data when creating a new task."""
+
+        LOG.info("%s.perform_create: %s", str(self), str(serializer))
         serializer.save()
 
 class SelectTemplateTaskDetailsView(generics.RetrieveDestroyAPIView):
