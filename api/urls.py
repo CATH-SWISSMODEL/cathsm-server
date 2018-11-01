3b111dfe8d8f8137d6e7748d69e616c5947540ac
sillitoe <ian@sillit.com>
2018-11-01 19:55:56 +0000

    use urls for create / status / results

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,21 @@
-# api/urls.py
+"""CATH API Urls"""
 
-from django.conf.urls import url, include
-from .views import SelectTemplateTaskCreateView, SelectTemplateTaskDetailsView
+from django.conf.urls import url
+from rest_framework.authtoken import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from .views import (SelectTemplateTaskCreateView, SelectTemplateTaskStatusView, 
+    SelectTemplateTaskResultsView)
+
+app_name = "api"
 urlpatterns = {
-    url(r'^select-template/$', SelectTemplateTaskCreateView.as_view(), name="create_selecttemplate"),
-    url(r'^select-template/(?P<pk>[0-9]+)/$', SelectTemplateTaskDetailsView.as_view(), name="details_selecttemplate"),
+    url(r'^api-auth-token/', views.obtain_auth_token, name="auth_token"),
+    url(r'^select-template/$',
+        SelectTemplateTaskCreateView.as_view(), name="create_selecttemplate"),
+    url(r'^select-template/(?P<uuid>[0-9a-f\-]{32,40})/$',
+        SelectTemplateTaskStatusView.as_view(), name="status_selecttemplate"),
+    url(r'^select-template/(?P<uuid>[0-9a-f\-]{32,40})/results$',
+        SelectTemplateTaskResultsView.as_view(), name="results_selecttemplate"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
