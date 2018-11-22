6550d8beb635387ae9b7d1f146b3177ac7d4cd66
Ian Sillitoe <ian@sillit.com>
2018-11-22 00:16:48 +0000

    improve error logging

--- frontend/src/components/AuthTokenProvider.js
+++ frontend/src/components/AuthTokenProvider.js
@@ -1,15 +1,14 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 });
 
 class AuthTokenProvider extends Component {
-
   constructor(props) {
     super(props);
     this.state = {};
@@ -20,42 +19,45 @@ class AuthTokenProvider extends Component {
   }
 
   getAuthTokenAsync() {
-    return fetch(this.props.endpoint, {
-      method: 'POST',
+    const data = {
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         username: this.props.username,
-        password: this.props.password,
-      }),
-    })
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response;
-    })
-    .then((response) => {
-      return response.json();
-    })
-    .then((responseJson) => {
-      const token = responseJson['token']; 
-      if ( typeof(token) !== 'string' ) {
-        throw Error("failed to get token from response json", responseJson);
-      }
-      this.setState(() => ({ authToken: token }));
-      this.props.onChange(token);
-    })
-    .catch(error => {
-      const msg = `An error occurred when getting auth token: ${error}`;
-      console.error(msg, error);
-      this.props.onError(msg);
-    });
+        password: this.props.password
+      })
+    };
+
+    return fetch(this.props.endpoint, data)
+      .then(response => Promise.all([response.ok, response.json()]))
+      .then(([responseOk, body]) => {
+        if (responseOk) {
+          const token = body["token"];
+          if (typeof token !== "string") {
+            throw Error("failed to get token from response json", body);
+          }
+          this.setState(() => ({ authToken: token }));
+          this.props.onChange(token);
+        } else {
+          const msg = body["non_field_errors"]
+            ? body["non_field_errors"]
+            : "Unexpected error";
+          throw Error(msg);
+        }
+      })
+      .catch(error => {
+        const msg = `Failed to get auth token: ${error}`;
+        console.error(msg);
+        this.props.onError(msg);
+      });
   }
 
-  render() { return null; }
+  render() {
+    return null;
+  }
 }
 
 AuthTokenProvider.propTypes = {
@@ -65,7 +67,7 @@ AuthTokenProvider.propTypes = {
   password: PropTypes.string.isRequired,
 
   onError: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired
 };
 
 export default withStyles(styles)(AuthTokenProvider);
