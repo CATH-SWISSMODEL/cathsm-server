d675ab63498fcf98691d31b986c7a7ac764feec6
Ian Sillitoe <ian@sillit.com>
2018-11-22 00:18:21 +0000

    correct segments (+ linting)

--- frontend/src/components/FunfamMatchList.js
+++ frontend/src/components/FunfamMatchList.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import { withStyles, createMuiTheme } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import Button from '@material-ui/core/Button';
-import Badge from '@material-ui/core/Badge';
-import Chip from '@material-ui/core/Chip';
-import IconButton from '@material-ui/core/IconButton';
-import Radio from '@material-ui/core/Radio';
-import Tooltip from '@material-ui/core/Tooltip';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import { lighten } from '@material-ui/core/styles/colorManipulator';
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { withStyles, createMuiTheme } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import Badge from "@material-ui/core/Badge";
+import Chip from "@material-ui/core/Chip";
+import IconButton from "@material-ui/core/IconButton";
+import Radio from "@material-ui/core/Radio";
+import Tooltip from "@material-ui/core/Tooltip";
+import DeleteIcon from "@material-ui/icons/Delete";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import { lighten } from "@material-ui/core/styles/colorManipulator";
 
-import ScanMatchFigure from './ScanMatchFigure';
+import ScanMatchFigure from "./ScanMatchFigure";
 
 function desc(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -46,17 +46,18 @@ function stableSort(array, cmp) {
 }
 
 function getSorting(order, orderBy) {
-  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+  return order === "desc"
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
 }
 
 const rows = [
-  { id: 'funfam', numeric: false, label: 'Match' },
-  { id: 'description', numeric: false, label: 'FunFam Description' },
-  { id: 'members', numeric: true, label: 'Domains' },
-  { id: 'features', numeric: false, label: 'Features' },
-  { id: 'query_region', numeric: false, label: 'Query Location' },
-  { id: 'match_region', numeric: false, label: 'Match Location' },
-  { id: 'evalue', numeric: true, label: 'E-value' },
+  { id: "funfam", numeric: false, label: "Match" },
+  { id: "description", numeric: false, label: "FunFam Description" },
+  { id: "members", numeric: true, label: "Domains" },
+  { id: "features", numeric: false, label: "Features" },
+  { id: "query_region", numeric: false, label: "Location of matches" },
+  { id: "evalue", numeric: true, label: "E-value" }
 ];
 
 // FunfamMatchTableHead
@@ -77,12 +78,12 @@ class FunfamMatchTableHead extends React.Component {
               <TableCell
                 key={row.id}
                 numeric={row.numeric}
-                padding={row.disablePadding ? 'none' : 'default'}
+                padding={row.disablePadding ? "none" : "default"}
                 sortDirection={orderBy === row.id ? order : false}
               >
                 <Tooltip
                   title="Sort"
-                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  placement={row.numeric ? "bottom-end" : "bottom-start"}
                   enterDelay={300}
                 >
                   <TableSortLabel
@@ -106,34 +107,34 @@ FunfamMatchTableHead.propTypes = {
   onRequestSort: PropTypes.func.isRequired,
   order: PropTypes.string.isRequired,
   orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
+  rowCount: PropTypes.number.isRequired
 };
 
 // FunfamMatchTableToolbar
 
 const toolbarStyles = theme => ({
   root: {
-    paddingRight: theme.spacing.unit,
+    paddingRight: theme.spacing.unit
   },
   highlight:
-    theme.palette.type === 'light'
+    theme.palette.type === "light"
       ? {
           color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85)
         }
       : {
           color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
+          backgroundColor: theme.palette.secondary.dark
         },
   spacer: {
-    flex: '1 1 100%',
+    flex: "1 1 100%"
   },
   actions: {
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.secondary
   },
   title: {
-    flex: '0 0 auto',
-  },
+    flex: "0 0 auto"
+  }
 });
 
 let FunfamMatchTableToolbar = props => {
@@ -142,7 +143,7 @@ let FunfamMatchTableToolbar = props => {
   return (
     <Toolbar
       className={classNames(classes.root, {
-        [classes.highlight]: numSelected > 0,
+        [classes.highlight]: numSelected > 0
       })}
     >
       <div className={classes.title}>
@@ -178,7 +179,7 @@ let FunfamMatchTableToolbar = props => {
 
 FunfamMatchTableToolbar.propTypes = {
   classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
+  numSelected: PropTypes.number.isRequired
 };
 
 FunfamMatchTableToolbar = withStyles(toolbarStyles)(FunfamMatchTableToolbar);
@@ -187,42 +188,41 @@ FunfamMatchTableToolbar = withStyles(toolbarStyles)(FunfamMatchTableToolbar);
 
 const styles = theme => ({
   root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 1,
+    width: "100%",
+    marginTop: theme.spacing.unit * 1
   },
   table: {
-    minWidth: 1020,
+    minWidth: 1020
   },
   tableWrapper: {
-    overflowX: 'auto',
-  },
+    overflowX: "auto"
+  }
 });
 
 class FunfamMatchList extends React.Component {
   state = {
-    order: 'asc',
-    orderBy: 'evalue',
+    order: "asc",
+    orderBy: "evalue",
     selected: [],
     page: 0,
-    rowsPerPage: 25,
+    rowsPerPage: 25
   };
 
   componentDidMount() {
-    const { scan, queryId, querySequence } = this.props;
-    this.setState({ scan, queryId, querySequence });
+    const { scanResult, queryId, querySequence } = this.props;
+    this.setState({ scanResult, queryId, querySequence });
   }
 
   handleRequestSort = (event, property) => {
     const orderBy = property;
-    let order = 'desc';
+    let order = "desc";
 
-    if (this.state.orderBy === property && this.state.order === 'desc') {
-      order = 'asc';
+    if (this.state.orderBy === property && this.state.order === "desc") {
+      order = "asc";
     }
 
     this.setState({ order, orderBy });
   };
-
 
   handleClick = (event, id) => {
     const { selected } = this.state;
@@ -238,7 +238,7 @@ class FunfamMatchList extends React.Component {
     } else if (selectedIndex > 0) {
       newSelected = newSelected.concat(
         selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
+        selected.slice(selectedIndex + 1)
       );
     }
 
@@ -257,26 +257,38 @@ class FunfamMatchList extends React.Component {
 
   render() {
     const { classes } = this.props;
-    const { scan, order, orderBy, selected, rowsPerPage, page, radio, queryId, querySequence } = this.state;
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+    const {
+      scanResult,
+      order,
+      orderBy,
+      selected,
+      rowsPerPage,
+      page,
+      radio,
+      queryId,
+      querySequence
+    } = this.state;
+    const hits = scanResult ? scanResult.hits : [];
+    const emptyRows =
+      rowsPerPage - Math.min(rowsPerPage, hits.length - page * rowsPerPage);
 
-    const result = scan.results[0];
-    const data = result.hits.map( hit => {
+    const data = hits.map(hit => {
       return {
+        id: hit.matchId,
         funfam: hit.matchId,
         description: hit.matchDescription,
         members: hit.funfamMembers,
-        uniq_ec_terms: hit.matchEcCount, 
-        uniq_go_terms: hit.matchGoCount, 
+        uniq_ec_terms: hit.matchEcCount,
+        uniq_go_terms: hit.matchGoCount,
         evalue: hit.significance,
-        segments: hit.hsps.map((hit, idx) => {
-          return { id: idx, start: hit.query_start, end: hit.query_end };
-        }),
+        segments: hit.hsps.map((hsp, idx) => {
+          return { id: idx, start: hsp.queryStart, end: hsp.queryEnd };
+        })
       };
     });
 
     return (
-      <Paper className={classes.root} >
+      <Paper className={classes.root}>
         <FunfamMatchTableToolbar numSelected={selected.length} />
         <div className={classes.tableWrapper}>
           <Table className={classes.table} aria-labelledby="tableTitle">
@@ -285,7 +297,7 @@ class FunfamMatchList extends React.Component {
               order={order}
               orderBy={orderBy}
               onRequestSort={this.handleRequestSort}
-              rowCount={scan.queryLength()}
+              rowCount={hits.length}
             />
             <TableBody>
               {stableSort(data, getSorting(order, orderBy))
@@ -308,11 +320,15 @@ class FunfamMatchList extends React.Component {
                       <TableCell>{n.description}</TableCell>
                       <TableCell>{n.members}</TableCell>
                       <TableCell>
-                      {n.uniq_ec_terms && 
-                        <Badge badgeContent={n.uniq_ec_terms} color="primary" className={classes.ec}>
-                          <Chip label="EC" className={classes.ec} />
-                        </Badge>
-                      }
+                        {n.uniq_ec_terms && (
+                          <Badge
+                            badgeContent={n.uniq_ec_terms}
+                            color="primary"
+                            className={classes.ec}
+                          >
+                            <Chip label="EC" className={classes.ec} />
+                          </Badge>
+                        )}
                       </TableCell>
                       <TableCell>
                         <ScanMatchFigure
@@ -323,26 +339,26 @@ class FunfamMatchList extends React.Component {
                       </TableCell>
                       <TableCell numeric>{n.evalue}</TableCell>
                     </TableRow>
-                );
-              })}
+                  );
+                })}
             </TableBody>
           </Table>
         </div>
         <TablePagination
-            rowsPerPageOptions={[10, 25, 50]}
-            component="div"
-            count={data.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            backIconButtonProps={{
-              'aria-label': 'Previous Page',
-            }}
-            nextIconButtonProps={{
-              'aria-label': 'Next Page',
-            }}
-            onChangePage={this.handleChangePage}
-            onChangeRowsPerPage={this.handleChangeRowsPerPage}
-          />
+          rowsPerPageOptions={[10, 25, 50]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          backIconButtonProps={{
+            "aria-label": "Previous Page"
+          }}
+          nextIconButtonProps={{
+            "aria-label": "Next Page"
+          }}
+          onChangePage={this.handleChangePage}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+        />
       </Paper>
     );
   }
@@ -350,10 +366,9 @@ class FunfamMatchList extends React.Component {
 
 FunfamMatchList.propTypes = {
   classes: PropTypes.object.isRequired,
-  scan: PropTypes.object,
+  scanResult: PropTypes.object,
   queryId: PropTypes.string.isRequired,
-  querySequence: PropTypes.string.isRequired,
+  querySequence: PropTypes.string.isRequired
 };
-  
+
 export default withStyles(styles)(FunfamMatchList);
-  
