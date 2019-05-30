497ed048c41b201bc6789522bce481a69e402174
Ian Sillitoe <ian@sillit.com>
2019-05-30 23:39:35 +0100

    register new models

--- select_template_api/admin.py
+++ select_template_api/admin.py
@@ -1,6 +1,12 @@
+"""
+Organises the admin section of Django
+"""
+
 from django.contrib import admin
 
 # Register your models here.
-from .models import SelectTemplateTask
+from .models import SelectTemplateTask, SelectTemplateHit, SelectTemplateAlignment
 
 admin.site.register(SelectTemplateTask)
+admin.site.register(SelectTemplateHit)
+admin.site.register(SelectTemplateAlignment)
