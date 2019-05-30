2453dae84d55ca293742357f0791d1ee94583afe
Ian Sillitoe <ian@sillit.com>
2019-05-30 23:39:43 +0100

    add errors

--- select_template_api/errors.py
+++ select_template_api/errors.py
@@ -1,3 +1,17 @@
+"""
+Custom errors
+"""
+
 
 class NoStructureDomainsError(Exception):
+    """
+    No structural domains in alignment
+    """
+    pass
+
+
+class DiscontinuousDomainError(Exception):
+    """
+    Not able to process discontinuous domains
+    """
     pass
