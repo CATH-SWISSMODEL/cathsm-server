62db514a6eeb9721f479d489ca048647f8bfb2c9
Ian Sillitoe <ian@sillit.com>
2019-06-06 11:34:57 +0100

    use redis as backend cache

--- cathapi/settings/prod.py
+++ cathapi/settings/prod.py
@@ -17,15 +17,19 @@ DATABASES = {
 ALLOWED_HOSTS = ['mysite.com', ]
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "cathapi"
     }
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'mytestuser'
+EMAIL_HOST_PASSWORD = 'mytestpassword'
 EMAIL_USE_TLS = True
