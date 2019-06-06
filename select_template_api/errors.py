14ce91054c61af8688040ac5be4dbaf043d4435d
Ian Sillitoe <ian@sillit.com>
2019-06-06 11:35:07 +0100

    add errors

--- select_template_api/errors.py
+++ select_template_api/errors.py
@@ -15,3 +15,30 @@ class DiscontinuousDomainError(Exception):
     Not able to process discontinuous domains
     """
     pass
+
+
+class NoRemoteTaskIdError(Exception):
+    """
+    Task needs to have `remote_task_id` set for this process
+    """
+    pass
+
+
+class ParseDataError(Exception):
+    """
+    Failed to parse scan data
+    """
+
+
+class NoResultsDataError(Exception):
+    """
+    Task requires results data for this process
+    """
+    pass
+
+
+class TaskInErrorStateError(Exception):
+    """
+    Task is in an error state and cannot be processed further
+    """
+    pass
