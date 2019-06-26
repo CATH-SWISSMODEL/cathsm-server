45815d3ad9b3c6c4991320e26aa7e2909154733e
isillitoe <ian@sillit.com>
2019-06-26 21:07:22 +0100

    move towards postgres for production

--- cathapi/settings/prod.py
+++ cathapi/settings/prod.py
@@ -8,10 +8,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'postgres',
-        # 'USER': 'postgres',
-        # 'HOST': 'db',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'cathapi',
+        # 'USER': 'cathapiuser',
+        # 'HOST': 'localhost',
         # 'PORT': 5432,
     }
 }
