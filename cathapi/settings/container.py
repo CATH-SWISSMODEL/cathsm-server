ff951cc452841c544e0239ca7f4a045af37ae80b
Stefan Bienert <stefan.bienert@unibas.ch>
2019-06-27 13:30:51 +0200

    Running Celery by Docker Compose

---
+++ cathapi/settings/container.py
@@ -0,0 +1,31 @@
+"""Setting for development environment"""
+
+from .base import *
+
+DEBUG = True
+
+# Bend the Django cache to use the redis container
+CACHES["default"]["LOCATION"] = "redis://cathapi-redis:6379/1"
+
+# Bend the broker to the redis container
+BROKER_URL = 'redis://cathapi-redis:6379'
+CELERY_RESULT_BACKEND = 'redis://cathapi-redis:6379'
+
+# These settings make sure any tasks run in testing 
+# are run locally with the 'test' database
+CELERY_ALWAYS_EAGER = True
+TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+INSTALLED_APPS += [
+    #    'debug_toolbar',
+]
+
+#MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
