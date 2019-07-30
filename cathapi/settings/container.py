afc763f9db29ca847ebcd80a79b1f9bf42f8d3ff
Stefan Bienert <stefan.bienert@unibas.ch>
2019-07-30 11:24:49 +0200

    Ignore Django settings when running as Celery container

--- cathapi/settings/container.py
+++ cathapi/settings/container.py
@@ -1,11 +1,8 @@
 """Setting for development environment"""
 
+from decouple import config
+
 from .base import *
-
-DEBUG = True
-
-# Bend the Django cache to use the redis container
-CACHES["default"]["LOCATION"] = "redis://cathapi-redis:6379/1"
 
 # Bend the broker to the redis container
 BROKER_URL = 'redis://cathapi-redis:6379'
@@ -16,22 +13,43 @@ CELERY_RESULT_BACKEND = 'redis://cathapi-redis:6379'
 CELERY_ALWAYS_EAGER = True
 TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
 
+IS_CELERY = config('I_AM_CELERY', default=False, cast=bool)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/cathapi-data', 'db.sqlite3'),
-    }
-}
+if not IS_CELERY:
+  # These settings are only relevant for Django instances
+  DEBUG = True
 
-INSTALLED_APPS += [
-    #    'debug_toolbar',
-]
+  # Bend the Django cache to use the redis container
+  CACHES["default"]["LOCATION"] = "redis://cathapi-redis:6379/1"
 
-STATICFILES_DIRS = [
-    os.path.join('static/'),
-]
+  '''
+  DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': config('PG_NAME', default='cathapi', cast=str),
+          'USER': config('PG_USER', default='cathapiuser', cast=str),
+          'PASSWORD': config('POSTGRES_PASSWORD', cast=str),
+          'HOST': config('PG_HOST', default='postgres', cast=str),
+          'PORT': config('PG_PORT', default=5432, cast=int),
+      }
+   }
+  '''
 
-STATIC_ROOT = '/static'
+  DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': os.path.join('/cathapi-data', 'db.sqlite3'),
+      }
+  }
 
-#MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+  INSTALLED_APPS += [
+      #    'debug_toolbar',
+  ]
+
+  STATICFILES_DIRS = [
+      os.path.join('static/'),
+  ]
+
+  STATIC_ROOT = '/static'
+
+  #MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
